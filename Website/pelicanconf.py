479bae96d7ed01bc9672185213c7e8c7a14ae280
Julien Lengrand-Lambert <jlengrand@gmail.com>
2012-09-14 14:06:59 +0200

    Changes order of pages, using a new variable in pelicanconf

--- Website/pelicanconf.py
+++ Website/pelicanconf.py
@@ -15,6 +15,15 @@ LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
           ('Jinja2', 'http://jinja.pocoo.org'),
           ('You can modify those links in your config file', '#'),)
 
+PAGELINKS =  (('Ivolution Project', 'Ivolution.html'),
+          ('1 minute kick-off', 'OneMinuteTutorial.html'),
+          ('Downloads', 'Downloads.html'),
+          ('Installation', 'Installation.html'),
+          ('Main Interface', 'MainInterface.html'),
+          ('Settings Interface', 'Settings.html'),
+          ('Future Developments', 'FutureDevelopments.html'),
+          ('About', 'About.html'),)
+
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
